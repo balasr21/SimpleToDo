6ec0ab1d83b667678ae1889b6d9b9cdfae119a4e
bala <balamurugan.radhakrishnan@yapily.com>
2020-10-17 22:57:54 +0100

    added Node server for hosint

---
+++ client/build/precache-manifest.c3c39080144d58b833be8a210ac96730.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e1be49ccc684e0f9db46ebfcef9426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5606311a1747bb8b6b",
+    "url": "/static/css/main.d619c6e8.chunk.css"
+  },
+  {
+    "revision": "f67483e73a71f0c7f003",
+    "url": "/static/js/2.a341c626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a341c626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5606311a1747bb8b6b",
+    "url": "/static/js/main.57c12a47.chunk.js"
+  },
+  {
+    "revision": "c1a5c0727862051c6817",
+    "url": "/static/js/runtime-main.e065febb.js"
+  },
+  {
+    "revision": "74fc797d2d34f79f05a5255671eaeb25",
+    "url": "/static/media/add.74fc797d.svg"
+  },
+  {
+    "revision": "8a0c8c4e90d8ac7681629b333225fc86",
+    "url": "/static/media/search.8a0c8c4e.svg"
+  }
+]);
